ee81a8bfffc165b788bb328c650fc4c178be3c7d
JuanRamirez8034 <72540234+JuanRamirez8034@users.noreply.github.com>
2021-08-07 21:08:30 -0400

    Add files via upload

---
+++ js/acciones_abrir_cerrar.js
@@ -0,0 +1,10 @@
+        // Funcion abrir sidebar
+        function action_abrir() {
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("myOverlay").style.display = "block";
+        }
+        //Funcion cerrar sidebar
+        function action_cerrar() {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("myOverlay").style.display = "none";
+        }
